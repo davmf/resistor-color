23e12fb8ec2c2a49e5ee2a843b6f8ea5ac909636
David Fyfe <david@davidfyfe.net>
2023-02-28 08:53:41 +1100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,42 @@
-#[derive(Debug, PartialEq, Eq)]
+use int_enum::IntEnum;
+
+#[repr(u32)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone, IntEnum)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
 }
 
 pub fn color_to_value(_color: ResistorColor) -> u32 {
-    unimplemented!("convert a color into a numerical representation")
+    _color.int_value()
 }
 
 pub fn value_to_color_string(value: u32) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
-}
+    ResistorColor::from_int(value)
+        .map_or("",
+                |result|
+        match result {
+            ResistorColor::Black => "Black",
+            ResistorColor::Brown => "Brown",
+            ResistorColor::Red => "Red",
+            ResistorColor::Orange => "Orange",
+            ResistorColor::Yellow => "Yellow",
+            ResistorColor::Green => "Green",
+            ResistorColor::Blue => "Blue",
+            ResistorColor::Violet => "Violet",
+            ResistorColor::Grey => "Grey",
+            ResistorColor::White => "White",
+        }
+    }
+
 
 pub fn colors() -> Vec<ResistorColor> {
     unimplemented!("return a list of all the colors ordered by resistance")
